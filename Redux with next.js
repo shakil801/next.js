72d1b7c7f56f5f83915b6d31fe421db3c1d59f93
Shakil Khan <157675459+shakil801@users.noreply.github.com>
2025-12-05 00:37:43 +0600

    Create Redux with next.js

---
+++ Redux with next.js
@@ -0,0 +1,4 @@
+Redux is a JavaScript library designed for managing and centralizing application state. It is widely used with React.Redux provides a predictable state container,
+making it easier to manage complex application states by centralizing them in a single store.
+#Redux Toolkit Installation
+npm install @reduxjs/toolkit
